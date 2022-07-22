cb3b8175acca3a0ee13fecb7806201a0fa89af61
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2022-07-22 19:55:54 +0530

    Home Draft1 Carousel

--- kfc-clone/src/components/common/navbar.jsx
+++ kfc-clone/src/components/common/navbar.jsx
@@ -10,27 +10,29 @@ export default function Navbar(){
     
     return(
         <nav>
-            <div >
-                <img className={styles.redstrip} src={redLine} alt="redLine" />
+            <div>
+                <div style={{position:"fixed",height:"1.2rem",width:"100%",top:"0",visibility:"visible",backgroundColor:"white",zIndex:"100001"}}>
+                    <img className={styles.redstrip} src={redLine} alt="redLine" />
+                </div>
                 <div className={styles.firstDiv}>
                     <ul className={styles.firstSubDiv}>
                         <li className={styles.leftPartTopNav}>
                             <Link to="/"><img className={styles.mainLogo} src={mainlogo} alt="mainLogo" /></Link>
                             <Link to="/menu"><p>Menu</p></Link>
-                            <Link to="/"><p>Careers</p></Link>
-                            <Link to="/"><p>About</p></Link>
-                            <Link to="/"><p>Find A KFC </p> <img height={"10rem"} style={{ padding: "0px 5px", cursor: "pointer" }} src={findKFC} alt="" /></Link>
+                            <Link to="/#"><p>Careers</p></Link>
+                            <Link to="/#"><p>About</p></Link>
+                            <Link to="/#"><p>Find A KFC </p> <img height={"10rem"} style={{ padding: "0px 5px", cursor: "pointer" }} src={findKFC} alt="" /></Link>
                         </li>
                         <li className={styles.rightPartTopNav}>
                             <div className={styles.StartButton} ><Link to="/menu" style={{ color: "white", textDecoration: "none" }} >Start Order</Link></div>
                             <div>
-                                <Link to="/"><img height={"30rem"} style={{ padding: "0.3vw 1vw" }} src={signin} alt="" /></Link>
-                                <Link to="/"><img height={"30rem"} style={{ padding: "0.3vw 1vw" }} src={cart} alt="" /></Link>
+                                <Link to="/#"><img height={"30rem"} style={{ padding: "0.3vw 1vw" }} src={signin} alt="" /></Link>
+                                <Link to="/#"><img height={"30rem"} style={{ padding: "0.3vw 1vw" }} src={cart} alt="" /></Link>
                             </div>
                         </li>
                     </ul>
                     <div className={styles.secondFindNav}>
-                        <Link to={"/"} className={styles.secondFindNav1} style={{ fontWeight: "500", alignItems: "center" }}>
+                        <Link to="/#" className={styles.secondFindNav1} style={{ fontWeight: "500", alignItems: "center" }}>
                             <img height={"20rem"} style={{ padding: "2px 10px 0px 0px", cursor: "pointer", flexDirection: "row", alignItems: "center" }} src={location} alt="" />
                             Start an Order for Pickup or Delivery
                         </Link>
